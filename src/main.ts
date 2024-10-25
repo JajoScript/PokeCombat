2d06c9ab8884173968f3c4edf180f7ff78df916f
Javier Almarza <javieralmarzabucarey@gmail.com>
2024-10-25 11:44:53 -0300

    :heavy_plus_sign: Se agregan nuevas fuentes

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+// Fonts.
 import '@fontsource/poppins/200.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/700.css';
 import '@fontsource/poppins/800.css';
+
+import '@fontsource/press-start-2p';
+import '@fontsource-variable/handjet/full.css';
 
 const app = createApp(App)
 
