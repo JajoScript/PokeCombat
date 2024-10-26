3b6ac02bc447ad561e6ba8bf04111db1c152859a
Javier Almarza <javieralmarzabucarey@gmail.com>
2024-10-26 00:29:52 -0300

    :wastebasket: Limpieza de código

--- src/components/Teams/index.ts
+++ src/components/Teams/index.ts
@@ -1,15 +1,9 @@
 // 🛢️ Barrel.
-import MemberInfo from "./MemberInfo.vue"
-import TeamContainer from "./TeamContainer.vue"
-
 import PokemonCard from "./PokemonCard.vue"
 import PokemonStarters from "./PokemonStarters.vue"
 
 // Export 🐶.
 export {
-  MemberInfo,
-  TeamContainer,
-
   PokemonStarters,
   PokemonCard
 }
